8c6e7ccc43d1fac34f55ae4892c485f5b0929d4c
Ben Abbott <jaminscript@gmail.com>
2015-09-22 13:03:55 +1200

    Add nav bar

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,6 +8,20 @@
     <script src="js/vendor/modernizr.js"></script>
   </head>
   <body>
+
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/pokedex">Pokedex</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/faq">FAQ</a></li>
+        <li><a href="/auth/register">Register</a></li>
+        <li><a href="/auth/login">Login</a></li>
+        <li><a href="/pokecentre">Pokecentre</a></li>
+        <li><a href="/auth/logout">Logout</a></li>
+      </ul>
+    </nav>
     
     @yield('content')
     
